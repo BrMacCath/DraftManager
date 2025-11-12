97a44f8d1b0f9980586bd080d096bb9ea4226b83
BrMacCath <brian.collery@colostate.edu>
2025-11-12 15:33:48 +0000

    Refactored the check update fucntion for readability.

--- src/main.ts
+++ src/main.ts
@@ -41,42 +41,43 @@ export default class DraftManagerPlugin extends Plugin {
 			}
 			// Prepare frontmatter conditions.
 			const metadata = ctx?.metadataEditor.properties;
-			let completeStatus = metadata.filter( (property)=>{
+			const completeStatusTemp = metadata.filter( (property)=>{
 				return property["key"] == completeFrontmatterIndicator
 			} )
-			if(completeStatus.length ==1 && completeStatus[0]["value"]){
+			if(completeStatusTemp.length ==1 && completeStatusTemp[0]["value"]){
 				// Don't update
 				new Notice("This page is listed as complete")
 				return;
 			}
 			
-			let draftNum = metadata.filter( (property)=>{
+			let draftNumTemp = metadata.filter( (property)=>{
 				return property["key"] == draftNumIndicator;
 			} )
-			if(draftNum.length != 1){
+			if(draftNumTemp.length != 1){
 				new Notice("The meta data for the draft number has been made incorrectly."
 					+" You indicate your draft number"
 					+ " by " +draftNumIndicator
 				)
 				return;
 			}
-
-			let draftStyle= metadata.filter( (property:any)=>{
+			const draftNum = draftNumTemp[0]["value"]
+			let draftStyleTemp= metadata.filter( (property:any)=>{
 				return property["key"] == draftStyleIndicator;
 			} );
 
-			if(draftStyle.length != 1){
+			if(draftStyleTemp.length != 1){
 				new Notice("The meta data for the draft style has been made incorrectly." 
 					+" You indicate your draft style"
 					+ " by " +draftStyleIndicator
 				)
 				return;
 			}
+			const draftStyle = draftNumTemp[0]["value"]
 			let listedStyle = false;
 			
 			
 			draftStyleOptions.forEach((style)=>{
-				if(style == draftStyle[0]["value"]){
+				if(style == draftStyle){
 					listedStyle = true
 				}
 			} );
@@ -90,23 +91,24 @@ export default class DraftManagerPlugin extends Plugin {
 			}
 			
 			// This puts text at the end of the document
-			const [continueForward, currentDraft]= extractCurrentDraft(editor.getValue(),draftNum[0]["value"])
+			const [continueForward, currentDraft]= extractCurrentDraft(editor.getValue(),draftNum)
 			if(!continueForward){
-				new Notice("Cannot find current drafts heading " + createDraftTitle(draftNum[0][["value"]]))
+				new Notice("Cannot find current drafts heading " + createDraftTitle(draftNum))
 				return;
 			}
 			let newContent = "";
-			if(draftStyle[0]["value"] =="Peterson"){
+			// For the moment I am not allowing people to use different styles but that will be a future update.
+			if(draftStyle =="Peterson"){
 				// Create draft
-				if(draftNum[0][["value"]]>1){
-					newContent = createPetersonDraft(currentDraft,draftNum[0]["value"],this.settings.defaultFolderConditions)
+				if(draftNum>1){
+					newContent = createPetersonDraft(currentDraft,draftNum,this.settings.defaultFolderConditions)
 				} else{
 					newContent = createFromPetersonFirstDraft(currentDraft,this.settings.defaultFolderConditions)
 				}
 			}
-			else if(draftStyle[0]["value"] =="Blank") {
+			else if(draftStyle =="Blank") {
 				const newLine = "\n";
-				const newTitle = createDraftTitle(draftNum[0][["value"]] +1)
+				const newTitle = createDraftTitle(draftNum +1)
 				newContent = newLine+ newLine + newTitle+ newLine+newLine;
 			}else{
 				new Notice("Draft style is not recognised.")
@@ -116,7 +118,7 @@ export default class DraftManagerPlugin extends Plugin {
 			editor.replaceRange(newContent,{line:editor.lastLine(),ch:endOfPage.length})
 			
 			this.app.fileManager.processFrontMatter(ctx.file,(frontmatter)=>{
-				frontmatter["DraftNum"] = frontmatter["DraftNum"] + 1;
+				frontmatter[draftNumIndicator] = frontmatter[draftNumIndicator] + 1;
 			})
 		}})
 
