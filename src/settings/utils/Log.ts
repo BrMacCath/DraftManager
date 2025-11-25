909518ce08cca169e70b8d7230f1fafe1ea1b564
BrMacCath <brian.collery@colostate.edu>
2025-11-25 21:39:07 +0000

    Update template message

--- src/settings/utils/Log.ts
+++ src/settings/utils/Log.ts
@@ -2,21 +2,20 @@ import { Notice } from "obsidian";
 import { TemplaterError } from "./Error";
 
 export function log_update(msg: string): void {
-    const notice = new Notice("", 15000);
+    const notice = new Notice(`Templater update:${msg}`, 15000);
     // TODO: Find better way for this
     // @ts-ignore
-    notice.messageEl.innerHTML = `<b>Templater update</b>:<br/>${msg}`;
 }
 
 export function log_error(e: Error | TemplaterError): void {
-    const notice = new Notice("", 8000);
+    
     if (e instanceof TemplaterError && e.console_msg) {
         // TODO: Find a better way for this
         // @ts-ignore
-        notice.messageEl.innerHTML = `<b>Templater Error</b>:<br/>${e.message}<br/>Check console for more information`;
+        const notice = new Notice(`Templater Error: ${e.message}. Check console for more information`, 8000);
         console.error(`Templater Error:`, e.message, "\n", e.console_msg);
     } else {
         // @ts-ignore
-        notice.messageEl.innerHTML = `<b>Templater Error</b>:<br/>${e.message}`;
+        const notice = new Notice(`Templater Error: ${e.message}`, 8000);
     }
 }
