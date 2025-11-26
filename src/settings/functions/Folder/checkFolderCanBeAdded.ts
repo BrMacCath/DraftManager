aa7b4c252b05391dd87ab5213283097825b720fa
BrMacCath <brian.collery@colostate.edu>
2025-11-26 13:38:36 +0000

    Fixed data issue

--- src/settings/functions/Folder/checkFolderCanBeAdded.ts
+++ src/settings/functions/Folder/checkFolderCanBeAdded.ts
@@ -1,12 +1,13 @@
 import { Notice, TFolder } from "obsidian";
 import type DraftManagerPlugin from "src/main";
 import type draftConditions from "types/choices/draftConditions";
+import type FolderArrangement from "types/FolderTypes/folderArrangement";
 import { v4 } from "uuid";
 
 
-export function checkFolderCanBeAdded(new_folder:string,plugin:DraftManagerPlugin):void{
+export async function checkFolderCanBeAdded(new_folder:string,plugin:DraftManagerPlugin):Promise<void>{
     for(let i = 0; i <plugin.settings.folders.length; i++){
-        if (new_folder == plugin.settings.folders[i].name){
+        if (new_folder == plugin.settings.folders[i].folder.name){
             new Notice("This folder is already on the list");
             return;
         }
@@ -18,7 +19,9 @@ export function checkFolderCanBeAdded(new_folder:string,plugin:DraftManagerPlugi
     }
 
     // This is assigning not just the values but the memory too.
-    plugin.settings.folders.push({name:tfold.name,id:v4(),draftConditions:this.plugin.settings.defaultFolder,compileOutput:"",subFiles:[],subFolders:[],moveType:"As Is",extractType:"Content"});
+    const folderArrangement:FolderArrangement ={name:tfold.name,id:v4(),draftConditions:this.plugin.settings.defaultFolder,compileOutput:"",subFiles:[],subFolders:[],moveType:"As Is",extractType:"Content"};
+
+    plugin.settings.folders.push({folder:folderArrangement,id:v4(),displayName: tfold.name,basePath:tfold.path});
     await plugin.saveSettings();
     //plugin.settings.display();
 }
